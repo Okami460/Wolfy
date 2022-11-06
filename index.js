738a4b9d08598da681fe5d1ba7daba24004cf8a4
Okami460 <okami460@outlook.com>
2022-11-06 19:54:35 +0100

    Wolfy

--- index.js
+++ index.js
@@ -320,72 +320,72 @@ function addErrorLog(err) {
 }
 
 
-// client.on('error', error => client.logger.log(error, "error"));
-// client.on('warn', info => client.logger.log(info, "warn"));
-// process.on('unhandledRejection', error => { addErrorLog(error); client.logger.log("UNHANDLED_REJECTION\n" + error, "error") });
-// process.on('uncaughtException', error => {
-//   addErrorLog(error);
-//   client.logger.log("UNCAUGHT_EXCEPTION\n" + error, "error");
-//   client.logger.log("Uncaught Exception is detected, restarting...", "info");
-//   process.exit(1);
-// });
+client.on('error', error => client.logger.log(error, "error"));
+client.on('warn', info => client.logger.log(info, "warn"));
+process.on('unhandledRejection', error => { addErrorLog(error); client.logger.log("UNHANDLED_REJECTION\n" + error, "error") });
+process.on('uncaughtException', error => {
+  addErrorLog(error);
+  client.logger.log("UNCAUGHT_EXCEPTION\n" + error, "error");
+  client.logger.log("Uncaught Exception is detected, restarting...", "info");
+  process.exit(1);
+});
 
 
 
 
-(async function () {
+// (async function () {
 
-  setInterval(async () => {
+//   setInterval(async () => {
     
-    try {
-      let info = await axios.get("https://m.adkami.com/api/main?objet=news&bot=an-hactys")
-    
-    
-      let urlimage = info.data["episodes"][0]["image"]
-      let urlserie = info.data["episodes"][0]["url"]
-      urlserie = "https://" + urlserie.slice(15, -1)
-      let epname = info.data["episodes"][0]["title"]
-      let epinfo = info.data["episodes"][0]["info"]
-      let kami_back = info.data["episodes"][1]["title"]
-      let kami_back_ep = info.data["episodes"][1]["info"]
-      let kami_back_image = info.data["episodes"][1]["image"]
-      let kami_back_url = info.data["episodes"][0]["url"]
+//     try {
+//       let info = await axios.get("https://m.adkami.com/api/main?objet=news&bot=an-hactys")
     
     
+//       let urlimage = info.data["episodes"][0]["image"]
+//       let urlserie = info.data["episodes"][0]["url"]
+//       urlserie = "https://" + urlserie.slice(15, -1)
+//       let epname = info.data["episodes"][0]["title"]
+//       let epinfo = info.data["episodes"][0]["info"]
+//       let kami_back = info.data["episodes"][1]["title"]
+//       let kami_back_ep = info.data["episodes"][1]["info"]
+//       let kami_back_image = info.data["episodes"][1]["image"]
+//       let kami_back_url = info.data["episodes"][0]["url"]
     
-      const embed = new discord.MessageEmbed()
-        .setTitle(`${epname}`)
-        .setDescription(`${epinfo}\n${urlserie}`)
-        .setAuthor({ name: "team: " + info.data["episodes"][0]["team"], iconURL: "https://m.adkami.com/favicon2.png" })
-        .setImage(urlimage)
-        .setURL(urlserie)
-        .setFooter({ text: "ADKami" })
     
     
-      const anime = require("./botConfig/anime.json")
+//       const embed = new discord.MessageEmbed()
+//         .setTitle(`${epname}`)
+//         .setDescription(`${epinfo}\n${urlserie}`)
+//         .setAuthor({ name: "team: " + info.data["episodes"][0]["team"], iconURL: "https://m.adkami.com/favicon2.png" })
+//         .setImage(urlimage)
+//         .setURL(urlserie)
+//         .setFooter({ text: "ADKami" })
+    
+    
+//       const anime = require("./botConfig/anime.json")
     
     
 
-        if (!anime.anime_name.includes(`${epname} ${epinfo}`)) {
-          anime.anime_name.push(`${epname} ${epinfo}`)
+//         if (!anime.anime_name.includes(`${epname} ${epinfo}`)) {
+//           anime.anime_name.push(`${epname} ${epinfo}`)
     
-          writeFileSync("./botConfig/anime.json", JSON.stringify(anime, null, 4))
-          await client.channels.cache.get("929074477579505667").send({
-            embeds: [embed]
-          })
-        } else return
+//           writeFileSync("./botConfig/anime.json", JSON.stringify(anime, null, 4))
+//           await client.channels.cache.get("929074477579505667").send({
+//             embeds: [embed]
+//           })
+//         } else return
     
-      } catch (error) {
-        console.log(error)
-      }
+//       } catch (error) {
+//         console.log(error)
+//       }
 
-  }, 5000)
-
-
+//   }, 5000)
 
 
 
-})()
+
+
+// })()
 
 
 
