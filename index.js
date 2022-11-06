3c4e20dd1aa3b97bd92f219b77863c64c004d568
Okami460 <okami460@outlook.com>
2022-11-06 20:23:54 +0100

    Wolfy

--- index.js
+++ index.js
@@ -331,63 +331,67 @@ process.on('uncaughtException', error => {
 });
 
 
+(async () => {
+
+  async function adkami() {
 
 
-// (async function () {
 
-//   setInterval(async () => {
-    
-//     try {
-//       let info = await axios.get("https://m.adkami.com/api/main?objet=news&bot=an-hactys")
-    
-    
-//       let urlimage = info.data["episodes"][0]["image"]
-//       let urlserie = info.data["episodes"][0]["url"]
-//       urlserie = "https://" + urlserie.slice(15, -1)
-//       let epname = info.data["episodes"][0]["title"]
-//       let epinfo = info.data["episodes"][0]["info"]
-//       let kami_back = info.data["episodes"][1]["title"]
-//       let kami_back_ep = info.data["episodes"][1]["info"]
-//       let kami_back_image = info.data["episodes"][1]["image"]
-//       let kami_back_url = info.data["episodes"][0]["url"]
-    
-    
-    
-//       const embed = new discord.MessageEmbed()
-//         .setTitle(`${epname}`)
-//         .setDescription(`${epinfo}\n${urlserie}`)
-//         .setAuthor({ name: "team: " + info.data["episodes"][0]["team"], iconURL: "https://m.adkami.com/favicon2.png" })
-//         .setImage(urlimage)
-//         .setURL(urlserie)
-//         .setFooter({ text: "ADKami" })
-    
-    
-//       const anime = require("./botConfig/anime.json")
-    
-    
-
-//         if (!anime.anime_name.includes(`${epname} ${epinfo}`)) {
-//           anime.anime_name.push(`${epname} ${epinfo}`)
-    
-//           writeFileSync("./botConfig/anime.json", JSON.stringify(anime, null, 4))
-//           await client.channels.cache.get("929074477579505667").send({
-//             embeds: [embed]
-//           })
-//         } else return
-    
-//       } catch (error) {
-//         console.log(error)
-//       }
-
-//   }, 5000)
+    await axios.get("https://m.adkami.com/api/main?objet=news&bot=an-hactys").then(async (info) => {
 
 
 
 
 
-// })()
+      let urlimage = info.data["episodes"][0]["image"]
+      let urlserie = info.data["episodes"][0]["url"]
+      urlserie = "https://" + urlserie.slice(15, -1)
+      let epname = info.data["episodes"][0]["title"]
+      let epinfo = info.data["episodes"][0]["info"]
+      let kami_back = info.data["episodes"][1]["title"]
+      let kami_back_ep = info.data["episodes"][1]["info"]
+      let kami_back_image = info.data["episodes"][1]["image"]
+      let kami_back_url = info.data["episodes"][0]["url"]
 
 
+
+      const embed = new discord.MessageEmbed()
+        .setTitle(`${epname}`)
+        .setDescription(`${epinfo}\n${urlserie}`)
+        .setAuthor({ name: "team: " + info.data["episodes"][0]["team"], iconURL: "https://m.adkami.com/favicon2.png" })
+        .setImage(urlimage)
+        .setURL(urlserie)
+        .setFooter({ text: "ADKami" })
+
+
+      const anime = require("./botConfig/anime.json")
+
+
+      try {
+        if (!anime.anime_name.includes(`${epname} ${epinfo}`)) {
+          anime.anime_name.push(`${epname} ${epinfo}`)
+
+          writeFileSync("./botConfig/anime.json", JSON.stringify(anime, null, 4))
+          await client.channels.cache.get("929074477579505667").send({
+            embeds: [embed]
+          })
+        } else return
+
+      } catch (error) {
+        console.log(error)
+      }
+
+
+    }).catch(err => console.log(err))
+
+
+  }
+  setInterval(() => {
+
+    adkami()
+  }, 5000)
+
+})()
 
 
 
