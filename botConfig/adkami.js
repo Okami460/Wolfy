2eb0e673449c496d8a421b5acea8ee404854fafd
Okami460 <okami460@outlook.com>
2022-11-21 19:37:20 +0100

    Wolfy

--- botConfig/adkami.js
+++ botConfig/adkami.js
@@ -1,51 +1,51 @@
-async function adkami(client, axios, discord, fs) {
+// async function adkami(client, axios, discord, fs) {
 
-    await axios.get("https://m.adkami.com/api/main?objet=news&bot=an-hactys", {
-      headers: { 'User-Agent':'Axios 0.21.1' }
-    }).then(async info => {
+//     await axios.get("", {
+//       headers: { 'User-Agent':'Axios 0.21.1' }
+//     }).then(async info => {
   
   
   
-          let urlimage = info.data["episodes"][0]["image"]
-          let urlserie = info.data["episodes"][0]["url"]
-          urlserie = "https://" + urlserie.slice(15, -1)
-          let epname = info.data["episodes"][0]["title"]
-          let epinfo = info.data["episodes"][0]["info"]
+//           let urlimage = info.data["episodes"][0]["image"]
+//           let urlserie = info.data["episodes"][0]["url"]
+//           urlserie = "https://" + urlserie.slice(15, -1)
+//           let epname = info.data["episodes"][0]["title"]
+//           let epinfo = info.data["episodes"][0]["info"]
     
     
     
-          const embed = new discord.MessageEmbed()
-            .setTitle(`${epname}`)
-            .setDescription(`${epinfo}\n${urlserie}`)
-            .setAuthor({ name: "team: " + info.data["episodes"][0]["team"], iconURL: "https://m.adkami.com/favicon2.png" })
-            .setImage(urlimage)
-            .setURL(urlserie)
-            .setFooter({ text: "ADKami" })
+//           const embed = new discord.MessageEmbed()
+//             .setTitle(`${epname}`)
+//             .setDescription(`${epinfo}\n${urlserie}`)
+//             .setAuthor({ name: "team: " + info.data["episodes"][0]["team"], iconURL: "https://m.adkami.com/favicon2.png" })
+//             .setImage(urlimage)
+//             .setURL(urlserie)
+//             .setFooter({ text: "ADKami" })
     
     
     
-          const anime = require("./anime.json")
+//           const anime = require("./anime.json")
     
     
-          try {
-            if (!anime.anime_name.includes(`${epname} ${epinfo}`)) {
-              anime.anime_name.push(`${epname} ${epinfo}`)
+//           try {
+//             if (!anime.anime_name.includes(`${epname} ${epinfo}`)) {
+//               anime.anime_name.push(`${epname} ${epinfo}`)
     
-              await client.channels.cache.get("929074477579505667").send({
-                  embeds: [embed]
-              })
-              fs.writeFileSync("./botConfig/anime.json", JSON.stringify(anime, null, 4))
-            } else return
+//               await client.channels.cache.get("929074477579505667").send({
+//                   embeds: [embed]
+//               })
+//               fs.writeFileSync("./botConfig/anime.json", JSON.stringify(anime, null, 4))
+//             } else return
     
-          } catch (error) {
-          }
+//           } catch (error) {
+//           }
     
 
 
 
-    }).catch(err => console.log(err))
+//     }).catch(err => console.log(err))
 
   
-}
+// }
 
-module.exports = { adkami }
+// module.exports = { adkami }
--- index.js
+++ index.js
@@ -60,55 +60,55 @@ require(`./botConfig/2048`)(client)
 
 
 
-// const MAX_ERROR_LOGS_SIZE = 5
+const MAX_ERROR_LOGS_SIZE = 5
 
-// function addErrorLog(err) {
-//   if (!require("fs").existsSync("botConfig/errorLogs.json")) require("fs").writeFileSync("botConfig/errorLogs.json", JSON.stringify([]));
-//   let logs = JSON.parse(require("fs").readFileSync("botConfig/errorLogs.json", "utf-8"));
-//   while (logs.length >= MAX_ERROR_LOGS_SIZE) logs.shift();
-//   logs.push({
-//     date: { __parsed: Date.now(), raw: new Date() },
-//     message: err.message,
-//     code: err.code,
-//     name: err.name,
-//     description: err.description || false,
-//     origin: err.fileName || false,
-//     stack: err.stack.split(/\n/g).filter((ln) => !(/node:internal/).test(ln)).map((l) => {
-//       const target = l.trim().replace(/at (?:[^()]+)(.+)?/, "$1").trim();
-//       const parsed = ((target || "").match(/:([0-9]+):[0-9]+/))
-//       return ({
-//         at: l.trim().replace(/at ([^()]+)(.+)?/, "$1").trim(),
-//         __rawTarget: target,
-//         target: target.replace(/\\{2}/gim, "/").replace(/^\((.+)\)$/, "$1") || false,
-//         line: (parsed ? parsed[1] || 0 : 0),
-//         column: (parsed ? parsed[2] || 0 : 0),
-//         __raw__: l,
-//       })
-//     }),
-//   });
-
-
-//   try { require("fs").writeFileSync("botConfig/errorLogs.json", JSON.stringify(logs, null, 2)); } catch (_) { };
-//   return true;
-// }
+function addErrorLog(err) {
+  if (!require("fs").existsSync("botConfig/errorLogs.json")) require("fs").writeFileSync("botConfig/errorLogs.json", JSON.stringify([]));
+  let logs = JSON.parse(require("fs").readFileSync("botConfig/errorLogs.json", "utf-8"));
+  while (logs.length >= MAX_ERROR_LOGS_SIZE) logs.shift();
+  logs.push({
+    date: { __parsed: Date.now(), raw: new Date() },
+    message: err.message,
+    code: err.code,
+    name: err.name,
+    description: err.description || false,
+    origin: err.fileName || false,
+    stack: err.stack.split(/\n/g).filter((ln) => !(/node:internal/).test(ln)).map((l) => {
+      const target = l.trim().replace(/at (?:[^()]+)(.+)?/, "$1").trim();
+      const parsed = ((target || "").match(/:([0-9]+):[0-9]+/))
+      return ({
+        at: l.trim().replace(/at ([^()]+)(.+)?/, "$1").trim(),
+        __rawTarget: target,
+        target: target.replace(/\\{2}/gim, "/").replace(/^\((.+)\)$/, "$1") || false,
+        line: (parsed ? parsed[1] || 0 : 0),
+        column: (parsed ? parsed[2] || 0 : 0),
+        __raw__: l,
+      })
+    }),
+  });
 
 
-// client.on('error', error => client.logger.log(error, "error"));
-// client.on('warn', info => client.logger.log(info, "warn"));
-// process.on('unhandledRejection', error => { addErrorLog(error); client.logger.log("UNHANDLED_REJECTION\n" + error, "error") });
-// process.on('uncaughtException', error => {
-//   addErrorLog(error);
-//   client.logger.log("UNCAUGHT_EXCEPTION\n" + error, "error");
-//   client.logger.log("Uncaught Exception is detected, restarting...", "info");
-//   process.exit(1);
-// });
+  try { require("fs").writeFileSync("botConfig/errorLogs.json", JSON.stringify(logs, null, 2)); } catch (_) { };
+  return true;
+}
+
+
+client.on('error', error => client.logger.log(error, "error"));
+client.on('warn', info => client.logger.log(info, "warn"));
+process.on('unhandledRejection', error => { addErrorLog(error); client.logger.log("UNHANDLED_REJECTION\n" + error, "error") });
+process.on('uncaughtException', error => {
+  addErrorLog(error);
+  client.logger.log("UNCAUGHT_EXCEPTION\n" + error, "error");
+  client.logger.log("Uncaught Exception is detected, restarting...", "info");
+  process.exit(1);
+});
 
 
 
-setTimeout(() => {
+// setTimeout(() => {
   
-  adkami(client, axios, discord, fs)
-}, 5000)
+//   adkami(client, axios, discord, fs)
+// }, 5000)
 
 
 
